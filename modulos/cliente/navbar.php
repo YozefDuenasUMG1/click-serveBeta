29d2fc36eb052de2296ac85192b603d263e6d7ca
JrCay <git@github.com:joseuniversidad/EjerciciosJAVAJoseCayetano.git>
2025-05-10 13:21:18 -0300

    Agregacion de Carrito

--- modulos/cliente/navbar.php
+++ modulos/cliente/navbar.php
@@ -42,7 +42,7 @@ if (session_status() === PHP_SESSION_NONE) {
                 
                 <li class="nav-item">
                     <button id="toggle-carrito-nav" class="btn btn-secondary position-relative">
-                        Carrito
+                       <i class="fas fa-shopping-cart"></i>
                         <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" id="cart-count">0</span>
                     </button>
                 </li>
